c24237c5668811d958d111e60b18f7c88f8f409e
JihenHs <jihen.hasnaoui1996@gmail.com>
2022-06-13 15:14:46 +0100

    adding component checkpoint

---
+++ src/profile/Adress.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Adress() {
+  return <h5>Ksar Bardo, Béja</h5>;
+}
+
+export default Adress;
---
+++ src/profile/Photoprofile.js
@@ -0,0 +1,7 @@
+import React from "react";
+import pic from "../pic.jpeg";
+function photoprofile() {
+  return <img src={pic} alt="myphoto" width={250} />;
+}
+
+export default photoprofile;
